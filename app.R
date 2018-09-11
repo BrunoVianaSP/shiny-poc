de962ee9ed2205cb8dc1bc9da10a4a7e0dcb2065
Bruno Barbosa <bruno.barbosa@keyrus.com.br>
2018-09-10 21:50:46 -0300

    Implemente poc

--- app.R
+++ app.R
@@ -12,16 +12,19 @@ ui <- dashboardPage(
   
   
   
+  # SIDEBAR START
   dashboardSidebar(
     
     sidebarMenu(
       menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-      menuItem("Load Data", tabName = "load-data", icon = icon("th"))
+      menuItem("Load Data", tabName = "load-data", icon = icon("th")),
+      menuItem("CMC Reports", tabName = "cmc-reports", icon = icon("line-chart"),
+               menuSubItem('Categorization',
+                        tabName = 'submenu-categorization',
+                        icon = icon('glyphicon glyphicon-penci')))
     )
     
-    
-    
-  ), # Sidebar end
+  ), # SIDEBAR END
   
   
   
@@ -47,6 +50,11 @@ ui <- dashboardPage(
       
       
       
+      
+      
+      
+      
+      
       # Second tab content
       tabItem(tabName = "load-data",
               h2("Load Data Section"),
@@ -54,8 +62,23 @@ ui <- dashboardPage(
                 fileInput("FileInput", "Choose file")),
                 column(DT::dataTableOutput("srvDataTable"), width = 12))
               
+      ),
+      
+      
+      # Second tab submenu content
+      tabItem(tabName = "submenu-categorization",
+              h2("Categorization Report"),
+              fluidRow( box(
+                fileInput("FileInput", "Choose file")),
+                column(DT::dataTableOutput("srvDataTable"), width = 12))
+              
       )
-    )
+      
+      
+      
+      
+      
+    ) # tab items end
     
     
   ) # Body end
